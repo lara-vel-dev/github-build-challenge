dd0da0b30c71be74f5ae30fcfd39c32e02c509e7
santpalacios <santpalacios12@hotmail.com>
2025-01-19 17:56:55 -0600

    register
    Signed-off-by: santpalacios <santpalacios12@hotmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 // import MainPage from "./pages/MainPage"
 import LoginPage from "./pages/LoginPage"
-
+import RegisterPage from "./pages/RegisterPage"
 function App() {
   return (
     <>
       {/* <MainPage /> */}
-      <LoginPage/>
+     {/* <LoginPage/> */}
+      <RegisterPage/>
     </>
   )
 }
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,11 +4,27 @@ const LoginPage = () => {
       <>
         <main className="loginpage">
           <div className="loginpage__container">
-            <h1>Iniciar Sesión</h1>
-            
+            <form className="loginpageForm">
+              <h1>Iniciar seción</h1>
+
+                <label htmlFor="email">Correo electroníco</label><br/>
+                <input type="email" id="email" name="email" required /><br/>
+          
+                <label htmlFor="password" >Contraseña</label><br/>
+                <input type="password" id="password" name="password" required /><br/>
+                <a>¿Olvidaste tu contraseña?</a><br/>
+                 
+              <div className="btnLogin">
+              <button type="submit">Iniciar sesión</button>
+              <p>¿No tienes una cuenta? <a>Registrate</a></p>
+              </div>
+
+            </form>
           </div>
           <div className="loginpage__principalwave wave"></div>
           <div className="loginpage__secondarywave wave"></div>
+          <div className="loginpage__huevito loginpage__huevito1"></div>
+          <div className="loginpage__huevito loginpage__huevito2"></div>
         </main>
       </>
     );
