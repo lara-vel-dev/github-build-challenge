c6c25953e7615a74b0bb74c1e3984b193ce2f1b7
santpalacios <santpalacios12@hotmail.com>
2025-01-19 16:39:24 -0600

    login
    Signed-off-by: santpalacios <santpalacios12@hotmail.com>

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,14 @@
+
 const LoginPage = () => {
     return (
       <>
         <main className="loginpage">
-         
           <div className="loginpage__container">
-           
             <h1>Iniciar Sesión</h1>
-            <img
-              className="loginpage__container--image"
-              src="https://png.pngtree.com/png-clipart/20240318/original/pngtree-3d-render-of-turnip-chick-miniature-cute-character-png-image_14617717.png"
-              alt="Pollo animado"
-            />
+            
           </div>
+          <div className="loginpage__principalwave wave"></div>
+          <div className="loginpage__secondarywave wave"></div>
         </main>
       </>
     );
