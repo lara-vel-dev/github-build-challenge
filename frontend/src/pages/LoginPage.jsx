db24c3f1534840d2c528b276549521489349669f
santpalacios <santpalacios12@hotmail.com>
2025-01-19 13:40:21 -0600

    login
    Signed-off-by: santpalacios <santpalacios12@hotmail.com>

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,20 @@
 const LoginPage = () => {
     return (
       <>
-       <h1>Hola</h1>
+        <main className="loginpage">
+         
+          <div className="loginpage__container">
+           
+            <h1>Iniciar Sesión</h1>
+            <img
+              className="loginpage__container--image"
+              src="https://png.pngtree.com/png-clipart/20240318/original/pngtree-3d-render-of-turnip-chick-miniature-cute-character-png-image_14617717.png"
+              alt="Pollo animado"
+            />
+          </div>
+        </main>
       </>
     );
-  };
-  
-  export default LoginPage;
-  
+};
+
+export default LoginPage;
