6fa4584ebfb4ce2b990b51ea84e428220b6f0705
Daniela Lara <laradni@icloud.com>
2025-01-19 11:23:06 -0600

    (Add) Main Page styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import MainPage from "./pages/MainPage"
+
 function App() {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <MainPage />
     </>
   )
 }
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,24 @@
+import { CgProfile } from "react-icons/cg";
+
+const MainPage = () => {
+  return (
+    <>
+      <main className="mainpage">
+        <CgProfile className="mainpage__profile" />
+        <div className="mainpage__container">
+          <h1>Hola, [Nombre]</h1>
+          <img
+            className="mainpage__container--image"
+            src="https://png.pngtree.com/png-clipart/20240318/original/pngtree-3d-render-of-turnip-chick-miniature-cute-character-png-image_14617717.png"
+            alt="Pollo animado"
+          />
+        </div>
+
+        <div className="mainpage__principalwave wave"></div>
+        <div className="mainpage__secondarywave wave"></div>
+      </main>
+    </>
+  );
+};
+
+export default MainPage;
